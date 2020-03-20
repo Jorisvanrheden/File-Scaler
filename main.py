dc13c9760af696ac0bd82d95dacc47cb20204f64
Joris van Rheden <jorisvanrheden@hotmail.com>
2020-03-20 22:55:06 +0100

    finished

--- main.py
+++ main.py
@@ -5,32 +5,29 @@ import numpy as np
 
 maxFileSize = 2000000
 
-scale = 0.5
-
 inputDirectory = r"D:\repos\File Scaler\Input\\"
 outputDirectory = r"D:\repos\File Scaler\Output\\"
 
 files = os.listdir(inputDirectory)
 for file in files:
-    fullFilePath = inputDirectory + file
 
+    fullFilePath = inputDirectory + file
     fileSize = os.path.getsize(fullFilePath)
 
-    if fileSize > maxFileSize:
-        print("Exceeded!")
+    image = cv2.imread(fullFilePath)
 
+    if fileSize > maxFileSize:
         scaler = fileSize / maxFileSize
         dimensionScaler = math.sqrt(scaler)
-
-        image = cv2.imread(file)
 
         scaledWidth = int(image.shape[0] / dimensionScaler)
         scaledHeight = int(image.shape[1] / dimensionScaler)
         scaleDimensions = (scaledHeight, scaledWidth)
+
+        print(scaleDimensions)
+
         resizeImage = cv2.resize(image, scaleDimensions)
-
         cv2.imwrite(outputDirectory + file, resizeImage)
-
-print("Hello world")
-
-
+    else:
+        image = cv2.imread(fullFilePath)
+        cv2.imwrite(outputDirectory + file, image)
